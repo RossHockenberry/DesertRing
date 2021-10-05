9d4752c521354d82196cf1887e542a1a3c99bfe1
RossHockenberry <rossedward01.rh@gmail.com>
2021-10-05 18:12:57 -0400

    Initial commit.

---
+++ Tingies_0017.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1583950189899" ID="ID_612211213" MODIFIED="1583956779957" TEXT="Tingies 0017">
+<node CREATED="1583956742758" HGAP="34" ID="ID_1452328236" MODIFIED="1583956815176" POSITION="right" TEXT="Ancient Alien Base" VSHIFT="-6">
+<node CREATED="1583959949423" HGAP="24" ID="ID_1769084904" MODIFIED="1583960063939" TEXT="Giant transporter receiving Station" VSHIFT="-120"/>
+<node CREATED="1583960071247" ID="ID_123044816" MODIFIED="1583960234288" TEXT="Desert Covers Receiving Dish">
+<node CREATED="1583960109689" ID="ID_1716795650" MODIFIED="1583960213132" TEXT="Desert allows lines up with  Alien galaxy once a day"/>
+</node>
+<node CREATED="1583960244360" ID="ID_824267429" MODIFIED="1583960272427" TEXT="Lake was created for the Dory"/>
+<node CREATED="1583960275319" ID="ID_569930937" MODIFIED="1583960299074" TEXT="Most of Alien complex is below lake.">
+<node CREATED="1583960304527" ID="ID_507101566" MODIFIED="1583960318330" TEXT="Lake was also a Starship Base">
+<node CREATED="1583990720691" ID="ID_1521954048" MODIFIED="1583990824180" TEXT="Maybe a few ships left there"/>
+<node CREATED="1583990751289" ID="ID_1780865195" MODIFIED="1583990816552" TEXT="There will be enough to show it&apos;s purpose"/>
+</node>
+</node>
+</node>
+</node>
+</map>
